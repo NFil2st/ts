a4171e871663daca34626f5c343fcb94e51c726b
Civilight <pattarapon_ja@cmu.ac.th>
2025-09-30 16:05:35 +0700

    เทส

--- dist/Test1.js
+++ dist/Test1.js
@@ -2,9 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const { Utils } = require("./Utils");
 if (Utils.add(2, 3) === 5) {
-    console.log("ถูก");
+    console.log("5");
 }
 else {
-    console.log("ผิด");
+    console.log("32");
 }
 //# sourceMappingURL=Test1.js.map
