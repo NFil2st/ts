30866de412657262eb735a36b5e26e481f146d2d
Civilight <pattarapon_ja@cmu.ac.th>
2025-09-30 15:50:22 +0700

    ลอง

---
+++ dist/Utils.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function helloworld() {
+    console.log("Hello, World!");
+}
+function add(a, b) {
+    return a + b;
+}
+module.exports = { Utils: { helloworld, add } };
+//# sourceMappingURL=Utils.js.map
